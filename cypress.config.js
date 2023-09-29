a2ec85b8db5d2784d7cc817d3dce4c40bb5634f9
Anastasiya <makeeva.anastasya89@gmail.com>
2023-09-30 02:25:01 +0300

    projectId added

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "o9c7fd",
   e2e: {
     baseUrl: "https://petstore.swagger.io/v2",
     setupNodeEvents(on, config) {
